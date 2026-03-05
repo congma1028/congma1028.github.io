09bbe8fdea7d35a7c7e6122614d889a3917870dc
congma1028 <congma2015@gmail.com>
2026-03-04 21:27:30 -0600

    update stat 253

--- paper_agent_cong/src/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Daily paper assistant package."""
--- paper_agent_cong/src/papers.py
+++
@@ -1,7 +0,0 @@
-"""CLI entrypoint for Daily Paper Assistant."""
-
-from .pipeline_engine import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
